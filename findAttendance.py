eb79187cc3794780b587f7e3c5c85394a332372d
JPAbela <59853197+JPAbela@users.noreply.github.com>
2021-09-24 10:46:01 -0700

    add totalAbsents and totalExcused to do the same thing but with a and e

--- findAttendance.py
+++ findAttendance.py
@@ -19,7 +19,27 @@ def totalPresents() -> str:
             output += (str(line[0]) + ': ' + str(line[-3]) + '\n')
     return output
 
+def totalAbsents() -> str:
+    output = ''
+    with open('attendanceSheet.csv', 'r') as file:
+        for line in file:
+            line = line.strip().split(',')
+            if line[0] == 'Name':
+                continue
+            output += (str(line[0]) + ': ' + str(line[-2]) + '\n')
+    return output
+
+def totalExcused() -> str:
+    output = ''
+    with open('attendanceSheet.csv', 'r') as file:
+        for line in file:
+            line = line.strip().split(',')
+            if line[0] == 'Name':
+                continue
+            output += (str(line[0]) + ': ' + str(line[-1]) + '\n')
+    return output
+
 
 if __name__ == '__main__':
-    print(totalPresents())
+    print(totalExcused())
     # pass
