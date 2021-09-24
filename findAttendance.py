a3bc2183d8ece1e955d0f0181cc066f118c90296
JPAbela <59853197+JPAbela@users.noreply.github.com>
2021-09-24 10:44:32 -0700

    add totalPresents to print the number of times everyone was present

--- findAttendance.py
+++ findAttendance.py
@@ -9,6 +9,17 @@ def findAttendance() -> str:
 
     return 'Cast member not found'
 
+def totalPresents() -> str:
+    output = ''
+    with open('attendanceSheet.csv', 'r') as file:
+        for line in file:
+            line = line.strip().split(',')
+            if line[0] == 'Name':
+                continue
+            output += (str(line[0]) + ': ' + str(line[-3]) + '\n')
+    return output
+
+
 if __name__ == '__main__':
-    print(findAttendance())
+    print(totalPresents())
     # pass
